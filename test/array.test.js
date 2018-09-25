728c1aa3b2c323fd9cb46f539e91678a84425c47
Davidchhing <davidhchhing@gmail.com>
2018-09-25 16:21:53 -0700

    made .filter

--- test/array.test.js
+++ test/array.test.js
@@ -6,15 +6,27 @@ describe('test arrays',() => {
     let list;
 
     beforeEach(() => {
-        list = new List([3, 5, 7, 8]);
+        list = new List([3, 5, , 7, 8]);
     })
 
     it('this should map a new list array', () => {
         const mapResults = list.map(item => {
              return item * 2;
         })
-        assert.deepEqual(mapResults, [6,10,14,16])
+        assert.deepEqual(mapResults, [6, 10, , 14, 16,])
     })
+
+    it ('this should filter and return a new array', () => {
+        const filterResults = list.filter(item => {
+            return item >= 4;
+        })
+        assert.deepEqual(filterResults, [5, 7, 8])
+    })
+
+    it ('this returns the first element of the index', () => {
+        
+    })
+
 });
 
 
