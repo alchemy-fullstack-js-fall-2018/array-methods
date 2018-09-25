aac1ce399e925ded4c7804753d3a9df800a967d5
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-25 15:15:20 -0700

    filter second test passing

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -8,6 +8,7 @@ describe('map', () => {
     };
 
     describe('map method tests', () => {
+
         it('returns an array where each item\'s value is doubled', () => {
             const list = new List([1, 3, 5, 7]);
             const doubleValues = list.map(item => {
@@ -15,18 +16,17 @@ describe('map', () => {
             });
 
             assert.deepEqual(doubleValues, [2, 6, 10, 14]);
-        });       
-    });
-    
-    describe('map method tests', () => {
+        }); 
+
         it('returns an array where empty items are returned as empty in the same position', () => {
             const list = new List([1, 3, , 5, , 7]);
             const doubleValues = list.map(item => {
                 return isDouble(item);
             });
-
+    
             assert.deepEqual(doubleValues, [2, 6, , 10, , 14]);
         });       
+
     });
 
 });
@@ -38,6 +38,7 @@ describe('filter', () => {
     };
 
     describe('filter method tests', () => {
+
         it('returns an array containing only items that pass filter criteria', () => {
             const list = new List([1, 4, 5, 6, 7]);
             const filtered = list.filter(item => {
@@ -45,19 +46,19 @@ describe('filter', () => {
             });
 
             assert.deepEqual(filtered, [4, 6]);
+        });    
+
+        it('skips empty items and does not return them', () => {
+            const list = new List([, 4, 5, 6, , 7]);
+            const filtered = list.filter(item => {
+                return isEven(item);
+            });
+
+            assert.deepEqual(filtered, [4, 6]);
         });       
+
     });
     
-    // describe('map method tests', () => {
-    //     it('returns an array where empty items are returned as empty in the same position', () => {
-    //         const list = new List([1, 3, , 5, , 7]);
-    //         const doubleValues = list.map(item => {
-    //             return isDouble(item);
-    //         });
-
-    //         assert.deepEqual(doubleValues, [2, 6, , 10, , 14]);
-    //     });       
-    // });
 
 });
 
