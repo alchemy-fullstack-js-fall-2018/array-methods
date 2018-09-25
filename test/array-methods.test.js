eee51851928f8fab72999d0dc74fb47fb8d27d83
MikeBLambert <mlamb128@gmail.com>
2018-09-25 15:27:42 -0700

    Refactor map tests

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -4,35 +4,45 @@ const List = require('../lib/array-methods');
 
 describe('map', () => {
     describe('array method assumptions', () => {
-        it('creates a new array', () => {
-            const arr = [1, 2, 3];
-            const list = new List(arr);
-            const mappedList = list.map((item) => {
-                return item * 2;
+        let list;
+        describe('array without holes', () => {
+            beforeEach(() => {
+                const arr = [1, 2, 3];
+                list = new List(arr);
             });
-            assert.notEqual(mappedList, arr);
-            assert.deepEqual(mappedList, [2, 4, 6]);
+            
+            it('creates a new array', () => {
+                const mappedList = list.map((item) => {
+                    return item * 2;
+                });
+                assert.deepEqual(mappedList, [2, 4, 6]);
+            });
+    
+            it('new length of array has same length as original array', () => {
+                const mappedList = list.map((item) => {
+                    return item * 2;
+                });
+                assert.deepEqual(mappedList.length, 3);
+            });
+        });
+        
+        describe('array with holes', () => {
+            let list;
+            beforeEach(() => {
+                //eslint-disable-next-line
+                const arr = [1, 2, ,3];
+                list = new List(arr);
+            })
+
+            it('new array has holes if original array had holes', () => {
+                const mappedList = list.map((item) => {
+                    return item * 2;
+                });
+                //eslint-disable-next-line
+                assert.deepEqual(mappedList, [2, 4, ,6]);
+            });
+
         });
 
-        it('new array has holes if original array had holes', () => {
-            //eslint-disable-next-line
-            const arr = [1, 2, ,3];
-            const list = new List(arr);
-            const mappedList = list.map((item) => {
-                return item * 2;
-            });
-            assert.notEqual(mappedList, arr);
-            //eslint-disable-next-line
-            assert.deepEqual(mappedList, [2, 4, ,6]);
-        });
-
-        it('new length of array has same length as original array', () => {
-            const arr = [1, 2, 3];
-            const list = new List(arr);
-            const mappedList = list.map((item) => {
-                return item * 2;
-            });
-            assert.deepEqual(mappedList.length, 3);
-        });
     });
 });
