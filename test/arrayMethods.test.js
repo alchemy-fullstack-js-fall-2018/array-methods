8fc1723b6dc2309c0454f627dceb30eb21a26625
Ryan Luras <ryanluras@gmail.com>
2018-09-25 15:03:54 -0700

    find index test

--- test/arrayMethods.test.js
+++ test/arrayMethods.test.js
@@ -11,20 +11,29 @@ describe('array methods', () => {
 
     describe('map', () => {
         it('should be a direct copy of original array', () => {
-            const result = list.map(this.items, (n) => {
-                return n;
-            });
+            const result = list.map(this.items, (n) => n);
             assert.deepEqual(result, [1, 2, 3, 4]);
         });
     });
 
     describe('filter', () => {
         it('returns new array of true items', () => {
-            const result = list.filter(this.items, (n) => {
-                return n < 4;
-            });
+            const result = list.filter(this.items, (n) => n < 4);
             assert.deepEqual(result, [1, 2, 3]);
         });
     });
 
+    describe('findIndex', () => {
+        it('returns first truthy item in array', () => {
+            const result = list.findIndex(this.items, (n) => n > 3);
+            assert.deepEqual(result, 3);
+        });
+
+        it('returns -1 if item not found', () => {
+            const result = list.findIndex(this.items, (n) => n > 5);
+            assert.deepEqual(result, -1);
+        });
+    });
+
+    
 });
