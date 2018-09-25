8030c92710d1796f8f27367e95e892d580114780
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-25 15:11:13 -0700

    filter first test passing

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -31,6 +31,36 @@ describe('map', () => {
 
 });
 
+describe('filter', () => {
+
+    const isEven = item => {
+        return item % 2 === 0;
+    };
+
+    describe('filter method tests', () => {
+        it('returns an array containing only items that pass filter criteria', () => {
+            const list = new List([1, 4, 5, 6, 7]);
+            const filtered = list.filter(item => {
+                return isEven(item);
+            });
+
+            assert.deepEqual(filtered, [4, 6]);
+        });       
+    });
+    
+    // describe('map method tests', () => {
+    //     it('returns an array where empty items are returned as empty in the same position', () => {
+    //         const list = new List([1, 3, , 5, , 7]);
+    //         const doubleValues = list.map(item => {
+    //             return isDouble(item);
+    //         });
+
+    //         assert.deepEqual(doubleValues, [2, 6, , 10, , 14]);
+    //     });       
+    // });
+
+});
+
 describe('every', () => {
 
     const isEven = item => {
