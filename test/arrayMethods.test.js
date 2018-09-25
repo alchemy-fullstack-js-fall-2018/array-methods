23f0e3a8154712792c8a7fc31fdba0e0b6dcf5ff
Ryan Luras <ryanluras@gmail.com>
2018-09-25 15:39:42 -0700

    making linter happy

--- test/arrayMethods.test.js
+++ test/arrayMethods.test.js
@@ -6,12 +6,14 @@ describe('array methods', () => {
     let list;
     
     beforeEach(() => {
+        //eslint-disable-next-line
         list = new List([1, 2, , 3, 4]);
     });
 
     describe('map', () => {
         it('should be a direct copy of original array', () => {
             const result = list.map(this.items, (n) => n);
+            //eslint-disable-next-line
             assert.deepEqual(result, [1, 2, , 3, 4]);
         });
     });
