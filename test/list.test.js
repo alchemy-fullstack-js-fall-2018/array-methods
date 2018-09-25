323c20826c6b712cf7df6e518d692124db05d197
Jack <ajackofsometrades@gmail.com>
2018-09-25 13:37:29 -0700

    test receiving callback

--- test/list.test.js
+++ test/list.test.js
@@ -5,7 +5,9 @@ describe('map', () => {
 
     it('returns an array of the same length', () => {
         const list = new List([1, 2, 3]);
-        const mappedList = list.map();
+        const mappedList = list.map(item => {
+            return item;
+        });
         assert.equal(list.items.length, mappedList.length);
     });
 
