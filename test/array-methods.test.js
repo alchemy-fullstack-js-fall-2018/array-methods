b6fcf4d0cba7441aa12a7888f977c551d32db564
Sophie Graham <sophiergraham1@gmail.com>
2018-10-03 17:28:50 -0700

    passed 'every' test and js

--- test/array-methods.test.js
+++ test/array-methods.test.js
@@ -24,8 +24,12 @@ describe('assumptions', () => {
         });
         assert.deepEqual(firstTrue, 1);
     });
-    it('reduce', () => {
-        // const numbers = [1, 2, 3];
+    it('every', () => {
+        const numbers = [1, 1, 1];
+        const allTrue = numbers.every(item => {
+            return item === 1;
+        });
+        assert.equal(allTrue, true);
     
     });
 
@@ -34,32 +38,40 @@ describe('assumptions', () => {
 
 describe('list class', () => {
     it('map', () => {
-        const numbers = new List ([1, 2, 3]);
+        const numbers = new List([1, 2, 3]);
         const doubles = numbers.map(item => {
             return item * 2;
         });
         assert.deepEqual(doubles, [2, 4, 6]);
     });
     it('filter', () => {
-        const numbers = new List ([1, 2, 3]);
+        const numbers = new List([1, 2, 3]);
         const filtered = numbers.filter(item => {
             return item > 1;
         });
         assert.deepEqual(filtered, [2, 3]);
     });
     it('findIndex', () => {
-        const numbers = new List ([1, 2, 3]);
+        const numbers = new List([1, 2, 3]);
         const firstTrue = numbers.findIndex(item => {
             return item > 1;
         });
         assert.deepEqual(firstTrue, 1);
     });
     it('findIndex no matches', () => {
-        const numbers = new List ([1, 2, 3]);
+        const numbers = new List([1, 2, 3]);
         const firstTrue = numbers.findIndex(item => {
             return item < 1;
         });
         assert.deepEqual(firstTrue, -1);
+    });
+    it('every', () => {
+        const numbers = new List([1, 1, 1]);
+        const allTrue = numbers.every(item => {
+            return item === 1;
+        });
+        assert.equal(allTrue, true);
+    
     });
 
 });
